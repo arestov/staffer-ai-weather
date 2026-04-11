219b3cf76e3d134b734b7a95bb68fb5deb24ea6e
Gleb Arestov <gr1b0k@gmail.com>
2026-04-11 17:48:55 +0300

    refactor: remove dead code

--- src/app/createBlankAppRootView.ts
+++
@@ -1,7 +0,0 @@
-import { view } from 'dkt/view.js'
-import { SessionRootView } from 'dkt-all/views/SessionRootView.js'
-
-export const BlankAppRootView = view({
-  extends: SessionRootView,
-  view_name: 'WeatherBlankAppRootView',
-})
