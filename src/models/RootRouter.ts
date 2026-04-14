5123482e92175467274dff30660fc2572cf1be80
Gleb Arestov <gr1b0k@gmail.com>
2026-04-14 16:01:52 +0300

    refactor(models): extract RootRouter model

---
+++ src/models/RootRouter.ts
@@ -0,0 +1,19 @@
+import { merge as mergeDcl } from 'dkt/dcl/merge.js'
+import { SelectedLocationPopoverRouter } from './SelectedLocationPopoverRouter'
+
+const RootRouter = mergeDcl({
+  model_name: 'weather_session_root',
+  attrs: {
+    sessionKey: ['input', null],
+    route: ['input', null],
+    closedAt: ['input', null],
+    isCommonRoot: ['input', false],
+  },
+  rels: {
+    'router-selectedLocationPopover': ['nest', [SelectedLocationPopoverRouter]],
+  },
+})
+
+export default RootRouter
+
+
