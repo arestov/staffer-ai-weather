1e2dec783d726c1cc502379bbb4811dfc0c6827a
Gleb Arestov <gr1b0k@gmail.com>
2026-04-14 16:15:41 +0300

    refactor(models): remove legacy SessionRoot module

--- src/app/SessionRoot.ts
+++
@@ -1,8 +0,0 @@
-import { SessionRoot as Base } from 'dkt-all/libs/provoda/bwlev/SessionRoot.js'
-import { model } from 'dkt/model.js'
-import RootRouter from './Routers/Root'
-
-export const SessionRoot = model({
-  extends: Base,
-  ...RootRouter,
-})
